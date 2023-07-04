087a2a29f6432fb8d92bdfd9b1ebf5b16cbad56f
joethompson <thompsonjoejj@gmail.com>
2023-07-04 14:46:40 +0100

    refactored function responses into utils file

--- teller_api/lib/teller_api/mfa.ex
+++ teller_api/lib/teller_api/mfa.ex
@@ -65,26 +65,6 @@ defmodule TellerApi.MFA do
         {:ok, unpacked} = response
         response_headers = unpacked.headers
 
-        case response do
-            {:ok, %HTTPoison.Response{status_code: 200, body: response_body}} ->
-                # Successful response
-                status_code = 200
-                status_text = get_status_text(status_code)
-                IO.puts("#{status_code} #{status_text}")
-                decoded_body = Poison.decode!(response_body)
-                outputResponse(response_headers)
-                IO.puts(Poison.encode!(decoded_body, pretty: true) <> "\n")
-                {:ok, decoded_body, response_headers}  # Return decoded response and headers
-
-            {:ok, %HTTPoison.Response{status_code: status_code, body: response_body}} ->
-                # Handle other response codes
-                status_text = get_status_text(status_code)
-                IO.puts("#{status_code} #{status_text}")
-                {:error, {status_code, response_body}, response_headers}
-
-            {:error, error} ->
-                # Handle request error
-                {:error, error, response_headers}
-        end
+        handle_response(response, response_headers)
     end
 end
