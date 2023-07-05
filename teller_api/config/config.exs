589df440591534988c28439ffe3ec13968c9f03e
joethompson <thompsonjoejj@gmail.com>
2023-07-05 23:13:37 +0100

    moved app contents back one folder and removed unnecessary files

--- teller_api/config/config.exs
+++
@@ -1,7 +0,0 @@
-import Config
-
-config :teller_api,
-  device_id: "MUQDFI57PM24Z4JT",
-  username: "green_lucky",
-  password: "papuanewguinea",
-  api_key: "HowManyGenServersDoesItTakeToCrackTheBank?"
--- teller_api/test/teller_api_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule TellerApiTest do
-  use ExUnit.Case
-  doctest TellerApi
-
-  test "greets the world" do
-    assert TellerApi.hello() == :world
-  end
-end
