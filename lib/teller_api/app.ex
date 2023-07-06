7c8ef13300a6a5c1f7448e001f24e553a7c4eab3
joethompson <thompsonjoejj@gmail.com>
2023-07-06 12:57:41 +0100

    added logout functionality

--- lib/teller_api/app.ex
+++ lib/teller_api/app.ex
@@ -4,34 +4,35 @@ defmodule TellerApi.App do
     import TellerApi.MFAVerify
     import TellerApi.Account
 
+
+    @initial_header_state [
+        {"teller-mission", "accepted!"},
+        {"user-agent", "Teller Bank iOS 2.0"},
+        {"api-key", "HowManyGenServersDoesItTakeToCrackTheBank?"},
+        {"device-id", "SM22X3DIAPKGRGUX"},
+        {"r-token", ""},
+        {"f-token", ""},
+        {"s-token", ""},
+        {"content-type", "application/json"},
+        {"accept", "application/json"}
+    ]
+
+    @initial_login_state %{
+        "username" => "green_lucky",
+        "password" => "papuanewguinea",
+    }
+
     def login do
         # device_id = IO.gets("Device ID: ") |> to_string() |> String.trim()
         # username = IO.gets("Username: ") |> to_string() |> String.trim()
         # password = IO.gets("Password: ") |> to_string() |> String.trim()
         # IO.puts("")
-
-        initial_header_state = [
-            {"teller-mission", "accepted!"},
-            {"user-agent", "Teller Bank iOS 2.0"},
-            {"api-key", "HowManyGenServersDoesItTakeToCrackTheBank?"},
-            {"device-id", "SM22X3DIAPKGRGUX"},
-            {"r-token", ""},
-            {"f-token", ""},
-            {"s-token", ""},
-            {"content-type", "application/json"},
-            {"accept", "application/json"}
-        ]
-
-        initial_login_state = %{
-            "username" => "green_lucky",
-            "password" => "papuanewguinea",
-        }
         
-        TellerApi.State.Header.start_link(initial_header_state)
-        TellerApi.State.Login.start_link(initial_login_state)
-        TellerApi.State.Body.start_link(initial_login_state)
+        TellerApi.State.Header.start_link(@initial_header_state)
+        TellerApi.State.Login.start_link(@initial_login_state)
+        TellerApi.State.Body.start_link(@initial_login_state)
 
-        signin(initial_header_state, initial_login_state)
+        signin(@initial_header_state, @initial_login_state)
 
         curHeaderState = TellerApi.State.Header.get_state()
         {_r_token_key, r_token} = Enum.find(curHeaderState, fn {key, _value} -> key == "r-token" end)
@@ -46,6 +47,30 @@ defmodule TellerApi.App do
         end
     end
 
+
+    def logout do
+        case Process.whereis(TellerApi.State.Header) do
+            nil -> 
+                IO.puts("Not logged in.")
+                :error
+
+            _pid ->
+                curHeaderState = TellerApi.State.Header.get_state()
+                {_r_token_key, r_token} = Enum.find(curHeaderState, fn {key, _value} -> key == "r-token" end)
+
+                if (r_token == "") do
+                    IO.puts("Not logged in.")
+                    :error
+                else
+                    TellerApi.State.Header.update_state(@initial_header_state)
+                    TellerApi.State.Body.update_state(@initial_login_state)
+                    TellerApi.State.Login.update_state(@initial_login_state)
+                    IO.puts("Logged out.")
+                end
+        end
+    end
+
+
     def account_balance do
         case Process.whereis(TellerApi.State.Header) do
             nil ->
@@ -53,7 +78,15 @@ defmodule TellerApi.App do
                 :error
 
             _pid ->
-                request_account_balance() 
+                curHeaderState = TellerApi.State.Header.get_state()
+                {_r_token_key, r_token} = Enum.find(curHeaderState, fn {key, _value} -> key == "r-token" end)
+
+                if (r_token == "") do
+                    IO.puts("Login required.")
+                    :error
+                else
+                    request_account_balance() 
+                end
         end
     end
 
