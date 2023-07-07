75f8b1ae06dc165c40e964eb0905bdd431ec6ec0
joethompson <thompsonjoejj@gmail.com>
2023-07-07 17:47:10 +0100

    code no longer uses hardcoded values

--- lib/teller_api/app.ex
+++ lib/teller_api/app.ex
@@ -4,35 +4,38 @@ defmodule TellerApi.App do
     import TellerApi.MFAVerify
     import TellerApi.Account
 
+    def login(device_id, username, password) do
+        initial_header_state = [
+            {"teller-mission", "accepted!"},
+            {"user-agent", "Teller Bank iOS 2.0"},
+            {"api-key", "HowManyGenServersDoesItTakeToCrackTheBank?"},
+            {"device-id", device_id},
+            {"r-token", ""},
+            {"f-token", ""},
+            {"s-token", ""},
+            {"content-type", "application/json"},
+            {"accept", "application/json"}
+        ]
 
-    @initial_header_state [
-        {"teller-mission", "accepted!"},
-        {"user-agent", "Teller Bank iOS 2.0"},
-        {"api-key", "HowManyGenServersDoesItTakeToCrackTheBank?"},
-        {"device-id", "QMZKQUZJLZX6T5ZU"},
-        {"r-token", ""},
-        {"f-token", ""},
-        {"s-token", ""},
-        {"content-type", "application/json"},
-        {"accept", "application/json"}
-    ]
+        initial_login_state = %{
+            "username" => username,
+            "password" => password,
+        }
 
-    @initial_login_state %{
-        "username" => "green_lucky",
-        "password" => "papuanewguinea",
-    }
+        case Process.whereis(TellerApi.State.Header) do
+            nil -> 
+                TellerApi.State.Header.start_link(initial_header_state)
+                TellerApi.State.Login.start_link(initial_login_state)
+                TellerApi.State.Body.start_link(initial_login_state)
 
-    def login do
-        # device_id = IO.gets("Device ID: ") |> to_string() |> String.trim()
-        # username = IO.gets("Username: ") |> to_string() |> String.trim()
-        # password = IO.gets("Password: ") |> to_string() |> String.trim()
-        # IO.puts("")
-        
-        TellerApi.State.Header.start_link(@initial_header_state)
-        TellerApi.State.Login.start_link(@initial_login_state)
-        TellerApi.State.Body.start_link(@initial_login_state)
+            _pid ->
 
-        signin(@initial_header_state, @initial_login_state)
+                TellerApi.State.Header.update_state(initial_header_state)
+                TellerApi.State.Body.update_state(initial_login_state)
+                TellerApi.State.Login.update_state(initial_login_state)
+        end
+
+        signin(initial_header_state, initial_login_state)
 
         curHeaderState = TellerApi.State.Header.get_state()
         {_r_token_key, r_token} = Enum.find(curHeaderState, fn {key, _value} -> key == "r-token" end)
@@ -55,22 +58,22 @@ defmodule TellerApi.App do
 
             _pid ->
                 curHeaderState = TellerApi.State.Header.get_state()
-                {_r_token_key, r_token} = Enum.find(curHeaderState, fn {key, _value} -> key == "r-token" end)
+                {_r_token_key, r_token} = Enum.find(curHeaderState, fn {key, _value} -> key == "r-token" end) || {"", ""}
 
                 if (r_token == "") do
                     IO.puts("Not logged in.")
                     :error
                 else
-                    TellerApi.State.Header.update_state(@initial_header_state)
-                    TellerApi.State.Body.update_state(@initial_login_state)
-                    TellerApi.State.Login.update_state(@initial_login_state)
+                    TellerApi.State.Header.update_state([])
+                    TellerApi.State.Body.update_state({})
+                    TellerApi.State.Login.update_state({})
                     IO.puts("Logged out.")
                 end
         end
     end
 
 
-    def account_details do
+    def account_details(account_id) do
         case Process.whereis(TellerApi.State.Header) do
             nil ->
                 IO.puts("Login required.")
@@ -84,13 +87,13 @@ defmodule TellerApi.App do
                     IO.puts("Login required.")
                     :error
                 else
-                    request_account_details() 
+                    request_account_details(account_id) 
                 end
         end
     end
 
 
-    def account_balance do
+    def account_balance(account_id) do
         case Process.whereis(TellerApi.State.Header) do
             nil ->
                 IO.puts("Login required.")
@@ -104,13 +107,13 @@ defmodule TellerApi.App do
                     IO.puts("Login required.")
                     :error
                 else
-                    request_account_balance() 
+                    request_account_balance(account_id) 
                 end
         end
     end
 
 
-    def account_transactions do
+    def account_transactions(account_id) do
         case Process.whereis(TellerApi.State.Header) do
             nil ->
                 IO.puts("Login required.")
@@ -124,10 +127,9 @@ defmodule TellerApi.App do
                     IO.puts("Login required.")
                     :error
                 else
-                    request_account_transactions() 
+                    request_account_transactions(account_id) 
                 end
         end
     end
-
 
 end
